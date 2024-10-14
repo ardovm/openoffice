ef861aef439c92e59bbb7eccd50e4ddbb750ccbd
Arrigo Marchiori <ardovm@yahoo.it>
2024-10-14 21:28:52 +0200

    icu: avoid icupkg to crash under Windows
    The destructor randomly crashes under WNT. Disabling it allows icupg to
    run and the whole icu package to be built successfully.

--- main/icu/makefile.mk
+++ main/icu/makefile.mk
@@ -44,7 +44,7 @@ TARFILE_ROOTDIR=icu
 
 # TODO file icu-mp.patch does not seem to be required
 PATCH_FILES=${TARFILE_NAME}.patch icu-win-layout.patch \
-	icu-format-security.patch
+	icu-format-security.patch icu-win-icupkg-avoid-crash.patch
 
 # ADDITIONAL_FILES=
 
